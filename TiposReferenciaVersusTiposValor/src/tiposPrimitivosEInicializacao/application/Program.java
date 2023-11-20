32117822da1da6a6b63865e4226a2f5d2264c49c
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-19 21:58:32 -0300

    reference types vs value types

---
+++ TiposReferenciaVersusTiposValor/src/tiposPrimitivosEInicializacao/application/Program.java
@@ -0,0 +1,16 @@
+package tiposPrimitivosEInicializacao.application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int p;
+
+//		System.out.println(p); // The local variable p may not have been initialized
+
+		p = 10;
+
+		System.out.println(p);
+	}
+
+}
