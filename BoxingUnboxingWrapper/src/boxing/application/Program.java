3aa3cb4431e6c8cd63441d762219552edeeda90b
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-21 14:53:01 -0300

    boxing

---
+++ BoxingUnboxingWrapper/src/boxing/application/Program.java
@@ -0,0 +1,20 @@
+package boxing.application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Boxing - É o processo de conversão de um objeto tipo valor para um objeto
+		 * tipo referência compatível
+		 */
+
+		int x = 20;
+
+		Object obj = x;
+
+		System.out.println(x);
+
+	}
+
+}
